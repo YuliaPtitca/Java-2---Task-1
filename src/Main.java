f51791d47bb3e4114b0c56aca8913ba6b5c3a4ea
unknown <qa.yulia.ptitca@yandex.ru>
2022-07-07 22:03:11 +0300

    Commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 12000;
+        int size = 20;
+        int mile = amount / size;
+        System.out.println("Стоимость билета" + " " + amount + " " + "руб.");
+        System.out.println("Ваш бонус равен" + " " + mile + " " + "миль.");
+
+    }
+}
